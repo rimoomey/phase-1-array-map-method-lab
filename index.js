0835b1dfbd152d83df557225e902c0fa5a403af9
Rimon Moomey <rimon.moomey@gmail.com>
2022-11-01 16:49:50 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((title) => makeTitle(title));
+};
+
+function makeTitle(string) {
+  const words = string.split(" ");
+  const capitalizedWords = words.map((w) => {
+    return w.slice(0, 1).toUpperCase() + w.slice(1);
+  });
+
+  return capitalizedWords.join(" ");
 }
